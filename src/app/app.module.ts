9ef0618043953f66f8b69e9e4ad8a0384b02ce7b
Antonio Perez <jpgtony@gmail.com>
2020-05-30 22:13:59 +0200

    otro update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
+// rutas
 import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
 
+import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from "./shared/footer/footer.component";
 import { PortafolioComponent } from "./pages/portafolio/portafolio.component";
+import { AboutComponent } from "./pages/about/about.component";
+import { ItemComponent } from "./pages/item/item.component";
 
 @NgModule({
   declarations: [
@@ -14,6 +17,8 @@ import { PortafolioComponent } from "./pages/portafolio/portafolio.component";
     HeaderComponent,
     FooterComponent,
     PortafolioComponent,
+    AboutComponent,
+    ItemComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
