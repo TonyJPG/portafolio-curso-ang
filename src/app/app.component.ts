8501a1dba097dd6f6b1bc8a04690b9cca3fe2358
Antonio Perez <jpgtony@gmail.com>
2020-06-01 14:59:57 +0200

    fin seccion 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { InfoPaginaService } from "./services/info-pagina.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'ng-portfolio';
+  constructor(public infoPaginaService: InfoPaginaService) {}
 }
